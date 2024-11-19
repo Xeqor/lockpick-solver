5dcaec23f91023dbc033e48a02a8b9d12cbf92e7
Xeq <108984002+Xeqor@users.noreply.github.com>
2024-11-19 18:37:01 +0800

    Created configurations.lua

---
+++ configurations.lua
@@ -0,0 +1,16 @@
+local Config = {}
+
+Config.END_REQUIREMENT = {} -- Superseded by FAST_MAP
+Config.STEPS_LIMIT = 100000000
+Config.USE_FAST_MAP = true
+Config.DEBUG = false
+
+Config.MAP = {} -- Superseded by FAST_MAP
+
+Config.FAST_MAP = [[
+
+
+
+]]
+
+return Config
